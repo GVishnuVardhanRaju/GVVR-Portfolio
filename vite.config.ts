835c56bd1102d58d7db71192e206354b2793a2bb
G Vishnu Vardhan Raju <thundervardhan@gmail.com>
2026-03-18 22:21:40 +0530

    Clean config and prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
     hmr: {
-      overlay: false,
+      overlay: false, // disables the error overlay in browser
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // allows @/ for src imports
     },
   },
-}));
+});
